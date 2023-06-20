99001dd7e0762e048e67b16a4ca9180168d2b128
Hector <hectorllorca7@gmail.com>
2023-06-20 23:26:45 +0200

    barrio dades

--- src/view/Barrio.jsx
+++ src/view/Barrio.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 
 const Barrio = () => {
+  const [select, setSelect] = useState();
+
   const barrioDetail = {
     name: "Nou Barris",
     alquiler: 250,
-    actividad: "feria de productos locales",
-    centroComercial: "Diagonal",
+    rentabilidad: 0.05,
   };
-  const actividades = ["pescaderia", "carniceria", "fruteria", "panaderia"];
+  let habitantes = 1000;
+  const actividades = ["pescaderias", "carnicerias", "fruterias", "panaderias"];
 
   return (
     <div className="flex flex-col gap-4 p-3">
@@ -16,18 +18,29 @@ const Barrio = () => {
         <ul className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
           {actividades.map((actividad, i) => (
             <li key={i}>
-              <a>{actividad}</a>
+              <span onClick={(e) => setSelect(e.target.innerText)}>
+                {actividad}
+              </span>
             </li>
           ))}
         </ul>
       </details>
       <div className="w-full p-6 bg-gradient-to-r from-purple-400 to-indigo-500 rounded-lg shadow-lg  text-white text-center">
         <h2 className="text-3xl font-bold mb-4">{barrioDetail.name}</h2>
+
+        <p className="mb-4 text-lg">Actividad principal: Hosteleria</p>
+        <p className="mb-4 text-lg">Actividad más demandada: Panaderia</p>
         <p className="mb-4 text-lg">
-          Centro Comercial: {barrioDetail.centroComercial}
+          Precio medio alquiler: {barrioDetail.alquiler}$
         </p>
-        <p className="mb-4 text-lg">Actividad: {barrioDetail.actividad}</p>
-        <p className="mb-4 text-lg">Alquiler: {barrioDetail.alquiler}</p>
+        {select && (
+          <p className="mb-4 text-lg">
+            Numero de {select} : 10 por cada {habitantes}
+          </p>
+        )}
+        {select && (
+          <p className="mb-4 text-lg">Rentabilidad del negocio: 0.5</p>
+        )}
       </div>
     </div>
   );
