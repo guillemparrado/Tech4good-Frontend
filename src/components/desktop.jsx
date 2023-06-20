5b86f7c92d818c0ad3fbdfa170420c19a335d369
Hector <hectorllorca7@gmail.com>
2023-06-20 22:15:48 +0200

    text color

--- src/components/desktop.jsx
+++ src/components/desktop.jsx
@@ -7,7 +7,7 @@ import Barrio from "../view/Barrio";
 const Desktop = () => {
   return (
     <div className="layout flex overflow-auto">
-      <div className="sidebar overflow-auto flex flex-col w-[250px] bg-black p-3 h-screen sticky top-0 gap-3 ">
+      <div className="sidebar overflow-auto flex flex-col w-[250px] bg-black text-base p-3 h-screen sticky top-0 gap-3 ">
         <Lista />
       </div>
       <div className="main w-full h-screen">
